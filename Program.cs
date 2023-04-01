a0784e980ad843400ebd4b62faf3c1440a209738
Administrator <klydefb@gmail.com>
2023-04-01 15:20:43 +0800

    Program structure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// STRING FORMATTING: Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+
+
+using System;
+
+
+
+
 
 namespace Tic_Tac_Toe
 {
@@ -6,7 +13,33 @@ namespace Tic_Tac_Toe
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Start game");
+            Console.WriteLine("Exit game");
+            Console.WriteLine("----------");
+            string userInput = Console.ReadLine();
+            int userInputConv = Convert.ToInt32(userInput);                                                             // Converting to integer type
+
+
+            
+            if (userInputConv == 1)
+                start_game();
+            else if (userInputConv == 2)
+                exit_game();
+
+        }
+        
+        
+        
+        
+        
+        static void start_game() 
+        {
+            Console.WriteLine("START");
+        }
+        
+        static void exit_game() 
+        {
+            Environment.Exit(1);
         }
     }
 }
